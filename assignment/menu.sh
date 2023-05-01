6dd420ae920d933a3b25f12367582212d636d6b6
KimTUD <124156277+KimTUD@users.noreply.github.com>
2023-05-01 10:48:09 +0000

    added menu

--- assignment/menu.sh
+++ assignment/menu.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+while true
+do
+    echo "    *Bash Commands*    "
+    echo ""
+    echo "1. List files"
+    echo "2. Show free disk space"
+    echo "3. Show system path"
+    echo "4. Display command history"
+    echo "5. Backup files"
+    echo "6. Exit"
+    echo ""
+    PS3="Enter your option: "
